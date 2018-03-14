88f08dd0013469f6cc21ef2f900c2c413639f90f
matt <matt@csx.lan>
2018-03-13 23:24:43 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        monstermatt2
+        <Header />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+    <header>
+      test head
+    </header>
+    );
+  }
+}
+
+export default Header;
