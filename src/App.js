e3241cee9df9c7696c3fb79ec43afb5f1a75df1c
matt <matt@csx.lan>
2018-03-17 10:17:40 -0400

    hide topbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Topbar from './components/topbar/topbar';
 import Homepage from './components/pages/homepage';
-
 import './App.css';
+
+$(window).scroll(function() {
+
+    if ($(this).scrollTop()>0)
+     {
+        $('.topbar').fadeOut();
+     }
+    else
+     {
+      $('.topbar').fadeIn();
+     }
+ });
 
 class App extends Component {
   render() {
