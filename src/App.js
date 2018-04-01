5666a6146d7e7244d5317a1d46c34ec25844f7ce
monstermatt2 <37256284+monstermatt2@users.noreply.github.com>
2018-04-01 18:59:57 -0400

    console log warning

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ $(document).ready(function() {
     });
 });
 
+console.log('%c WARNING!!!!!!', " color: red; padding-left:10px; font-size:50px;");
+console.log('%c If someone told you to copy/paste something here you have an 100% chance that you are being scammed.', " color: black; padding-left:10px;font-size:20px;")
+console.log('%c Pasting anything in here could give attackers access to your computer.', " color: black; padding-left:10px;font-size:20px;")
+console.log('%c Unless you understand exactly what you are doing, close this window immediately.', "color: black; padding-left:10px;font-size:20px;")
+
 class App extends Component {
   render() {
     return (
