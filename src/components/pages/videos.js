a187ca27479d4726cdebb7dd0838b29081a66b87
matt <matt@csx.lan>
2018-06-07 16:09:22 -0400

    cleanup

--- src/components/pages/videos.js
+++ src/components/pages/videos.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Vsidebar from "../vsidebar/vsidebar";
-import Test1 from "./video test pages/test1";
-import Test2 from "./video test pages/test2";
-import Test3 from "./video test pages/test3";
-
 
 class Videos extends Component {
   render() {
     return (
-    <Router>
       <div className="wrapper">
         <div classname="main">
-          <Vsidebar />
           <div className="video-list">
-            <Route exact path='/video/Test1' component={Test1} />
-            <Route exact path='/video/Test2' component={Test2} />
-            <Route exact path='/video/Test3' component={Test3} />
+          test
           </div>
         </div>
       </div>
-    </Router>
     );
   }
 }
--- src/components/vsidebar/vsidebar.js
+++ src/components/vsidebar/vsidebar.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
 
 class Vsidebar extends Component {
   render() {
     return (
-        <div className="sidebar">
-            <li><Link to="/video/Test1">test 1</Link></li>
-            <li><Link to="/video/Test2">test 2</Link></li>
-            <li><Link to="/video/Test3">test3</Link></li>
+        <div className="vsidebar">
+            test
         </div>
     );
   }
